5cbad74045973a7a8868b98d746fb70e31ad9e68
kate7-2 <katemate484@gmail.com>
2025-04-06 20:34:39 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,8 +5,8 @@ int countPairs1(int *arr, int len, int value) {
     for (int j = i+1; j < len; j++) {
       if (arr[i] + arr[j] == value) {
         n++;
-      }
-     }
+	  }
+	}
   }
   return n;
 }
