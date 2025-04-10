1bf960630ff4791108a1c5b89c51109e758b12b2
kate7-2 <katemate484@gmail.com>
2025-04-10 04:10:46 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,7 +3,7 @@ int countPairs1(int* arr, int len, int value) {
   int count = 0;
   for (int i = 0; i < len - 1; ++i) {
     for (int j = i + 1; j < len; ++j) {
-     if (arr[i] + arr[j] == value) {     
+     if (arr[i] + arr[j] == value) {
        count++;
      } else if (arr[i] + arr[j] > value) {
        break;
@@ -76,5 +76,6 @@ int countPairs3(int* arr, int len, int value) {
     if (left != -1 && right != -1 && right >= left) {
       result += (right - left + 1);
     }
+  }
   return result;
 }
